4d34b1bbd6eb5b1b4f09fea228f7ec9ee45e22fa
Matthias Jouanneaux <mjoux@nvidia.com>
2026-02-19 16:57:51 +0100

    experiments with cuda caching allocator

--- c10/cuda/CUDACustomizableAllocator.cpp
+++ c10/cuda/CUDACustomizableAllocator.cpp
@@ -1,4 +1,4 @@
-#include <c10/cuda/CUDACachingAllocator.h>
+#include <c10/cuda/CUDACustomizableAllocator.h>
 
 #include <c10/core/impl/GPUTrace.h>
 #include <c10/cuda/CUDAAllocatorConfig.h>
@@ -59,77 +59,40 @@ namespace c10 {
 // NOLINTNEXTLINE(misc-use-internal-linkage)
 C10_DEFINE_REGISTRY(FreeCudaMemoryCallbacksRegistry, FreeMemoryCallback)
 
-namespace cuda::CUDACachingAllocator {
+namespace cuda::CUDACustomizableAllocator {
 
 using namespace c10::CachingAllocator;
 using namespace c10::CachingDeviceAllocator;
 
 namespace Native {
 
-//
-// Yet another caching allocator for CUDA device allocations.
-//
-// - Allocations are associated with a stream. Once freed, blocks can be
-//   re-allocated on the same stream, but not on any other stream.
-// - The allocator attempts to find the smallest cached block that will fit the
-//   requested size. If the block is larger than the requested size, it may be
-//   split. If no block is found, the allocator will delegate to cudaMalloc.
-// - If the cudaMalloc fails, the allocator will attempt to free one cached
-//   block of sufficient size that is not split and retry the allocation.
-//   If this also fails, the allocator will attempt to free all cached blocks
-//   that are not split and retry the allocation.
-// - Large (>1MB) and small allocations are stored in separate pools.
-//   Small requests are packed into 2MB buffers. Large requests will use the
-//   smallest available free block or allocate a new block using cudaMalloc.
-// - To reduce fragmentation, requests between 1MB and 10MB will allocate and
-//   split a 20MB block, if no free block of sufficient size is available.
-// - To further reduce fragmentation, blocks >= max_split_size are not allowed
-//   to be split. These oversize cached blocks will still satisfy requests
-//   within 1MB of the oversize cached block size.
-//
-// With this allocator, allocations and frees should logically be considered
-// "usages" of the memory segment associated with streams, just like kernel
-// launches. The programmer must insert the proper synchronization if memory
-// segments are used from multiple streams.
-//
-// The library provides a recordStream() function to help insert the correct
-// synchronization when allocations are used on multiple streams. This will
-// ensure that the block is not reused before each recorded stream completes
-// work.
-//
+struct AllocatorConfig {
+  bool uses_expandable_segments;
+  bool uses_peer_access;
+  bool uses_caching;
+  int provided_alignment;
+};
 
-/**
- * Note [Interaction with CUDA graph capture]
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * Graph capture performs a dry run of a region of execution, freezing all CUDA
- * work (and virtual addresses used during that work) into a "graph." The graph
- * may be "replayed" like a single giant kernel, with greatly reduced CPU
- * overhead as well as modestly improved GPU performance.
- *
- * Because capture bakes in memory addresses, the memory used during capture
- * must be available for the graph to use during replay. DeviceCachingAllocator
- * assigns and frees memory eagerly and dynamically, so if we're not careful
- * about managing graphs' memory, at replay time those memory addresses could be
- * used by other tensors.
- *
- * To guarantee a graph's baked in addresses are safe to reuse in replay,
- * DeviceAllocator satisfies allocations from a graph-private memory pool during
- * capture, and doesn't begin cudaFreeing those addresses until the graph is
- * destroyed.
- *
- * Within the private pool, allocations are freed and reassigned as usual during
- * capture. Memory regions will be used in a consistent order during replay. So
- * a private pool doesn't use memory more wastefully than the default pools
- * during capture, but it does reserve its high-water mark of used memory away
- * from the default pools as long as the capture(s) it served survive
- * (regardless whether those captures are idle or replaying).
- *
- * CUDAGraph's requests for private pools are mediated by
- * DeviceAllocator::notifyCaptureBegin,
- *                  notifyCaptureAboutToEnd,
- *                  notifyCaptureEnded,
- *                  notifyCaptureDestroy.
- */
+struct AllocationParams {
+  bool is_successful;
+  size_t allocated_bytes;
+  void* ptr;
+};
+
+class CustomAllocator {
+  public:
+    virtual AllocationParams alloc(size_t nbytes, cudaStream_t stream) = 0;
+    virtual void free(AllocationParams params, cudaStream_t stream) = 0;
+    virtual void map(void* ptr, size_t nbytes, cudaStream_t stream) {
+      TORCH_CHECK_NOT_IMPLEMENTED(
+        config.uses_expandable_segments, "allocators using expandable segments must implement map.");
+    }
+    virtual void unmap(void* ptr, size_t nbytes, cudaStream_t stream) {
+      TORCH_CHECK_NOT_IMPLEMENTED(
+        config.uses_expandable_segments, "allocators using expandable segments must implement unmap.");
+    }
+  AllocatorConfig config;
+};
 
 static char SHAREABLE_HANDLE_VERSION = 2;
 enum ShareableHandleType : char {
@@ -1661,7 +1624,7 @@ class DeviceCachingAllocator {
       stats.oversize_allocations.increase(1);
 
     auto allocated_bytes_gauge =
-        STATIC_GAUGE(pytorch.CUDACachingAllocator.allocated_bytes);
+        STATIC_GAUGE(pytorch.CUDACustomizableAllocator.allocated_bytes);
     allocated_bytes_gauge.record(
         stats.allocated_bytes[static_cast<int64_t>(StatType::AGGREGATE)]
             .current);
@@ -1912,7 +1875,7 @@ class DeviceCachingAllocator {
       stats.allocated_bytes[stat_type].decrease(block->size);
     });
     auto allocated_bytes_gauge =
-        STATIC_GAUGE(pytorch.CUDACachingAllocator.allocated_bytes);
+        STATIC_GAUGE(pytorch.CUDACustomizableAllocator.allocated_bytes);
     allocated_bytes_gauge.record(
         stats.allocated_bytes[static_cast<int64_t>(StatType::AGGREGATE)]
             .current);
@@ -2392,7 +2355,7 @@ class DeviceCachingAllocator {
       }
     } else {
       // When snapshot is called with non-default mempool_id, we return
-      // all the blocks in the CUDACachingAllocator (as returned by
+      // all the blocks in the CUDACustomizableAllocator (as returned by
       // get_all_blocks).
       all_blocks = get_all_blocks();
     }
@@ -2792,7 +2755,7 @@ class DeviceCachingAllocator {
       stats.reserved_bytes[stat_type].increase(mapped_range.size);
     });
     auto reserved_bytes_gauge =
-        STATIC_GAUGE(pytorch.CUDACachingAllocator.reserved_bytes);
+        STATIC_GAUGE(pytorch.CUDACustomizableAllocator.reserved_bytes);
     reserved_bytes_gauge.record(
         stats.reserved_bytes[static_cast<int64_t>(StatType::AGGREGATE)]
             .current);
@@ -3249,7 +3212,7 @@ class DeviceCachingAllocator {
     if (size >= AcceleratorAllocatorConfig::max_split_size())
       stats.oversize_segments.increase(1);
     auto reserved_bytes_gauge =
-        STATIC_GAUGE(pytorch.CUDACachingAllocator.reserved_bytes);
+        STATIC_GAUGE(pytorch.CUDACustomizableAllocator.reserved_bytes);
     reserved_bytes_gauge.record(
         stats.reserved_bytes[static_cast<int64_t>(StatType::AGGREGATE)]
             .current);
@@ -3417,7 +3380,7 @@ class DeviceCachingAllocator {
       stats.reserved_bytes[stat_type].decrease(block->size);
     });
     auto reserved_bytes_gauge =
-        STATIC_GAUGE(pytorch.CUDACachingAllocator.reserved_bytes);
+        STATIC_GAUGE(pytorch.CUDACustomizableAllocator.reserved_bytes);
     reserved_bytes_gauge.record(
         stats.reserved_bytes[static_cast<int64_t>(StatType::AGGREGATE)]
             .current);
@@ -3477,7 +3440,7 @@ class DeviceCachingAllocator {
       stats.reserved_bytes[stat_type].decrease(unmapped.size);
     });
     auto reserved_bytes_gauge =
-        STATIC_GAUGE(pytorch.CUDACachingAllocator.reserved_bytes);
+        STATIC_GAUGE(pytorch.CUDACustomizableAllocator.reserved_bytes);
     reserved_bytes_gauge.record(
         stats.reserved_bytes[static_cast<int64_t>(StatType::AGGREGATE)]
             .current);
@@ -4519,5 +4482,5 @@ struct BackendStaticInitializer {
 
 std::atomic<CUDAAllocator*> allocator;
 static BackendStaticInitializer backend_static_initializer;
-} // namespace cuda::CUDACachingAllocator
+} // namespace cuda::CUDACustomizableAllocator
 } // namespace c10
